cd8b481d7776c8c014ef168cc6a9ff2a14a533b8
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-12-01 17:01:16 +0100

    REFACTOR: Add Javadoc comments to Account class

--- app/src/main/java/bankapp/Account.java
+++ app/src/main/java/bankapp/Account.java
@@ -1,12 +1,25 @@
 package bankapp;
 
+/**
+ * Represents a bank account with an account ID.
+ */
 public class Account {
-  private String accountId;
+    private String accountId;
 
+    /**
+     * Constructs an Account with the specified account ID.
+     *
+     * @param accountId the account ID
+     */
     public Account(String accountId) {
         this.accountId = accountId;
     }
 
+    /**
+     * Returns the account ID.
+     *
+     * @return the account ID
+     */
     public String getAccountId() {
         return accountId;
     }
