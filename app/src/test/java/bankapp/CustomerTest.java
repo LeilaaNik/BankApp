b4e0c639af62bebc125451f69095f80c710c1993
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-12-02 20:24:09 +0100

    RED: Add failing test for addAccount and getAccounts methods in CustomerTest class

--- app/src/test/java/bankapp/CustomerTest.java
+++ app/src/test/java/bankapp/CustomerTest.java
@@ -3,10 +3,22 @@ package bankapp;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 public class CustomerTest {
     @Test
     public void testGetCustomerId() {
         Customer customer = new Customer("cust1");
         assertEquals("cust1", customer.getCustomerId());
+    }
+
+    @Test
+    public void testAddAccount() {
+        Customer customer = new Customer("cust1");
+        Account account = new Account("acc1");
+        customer.addAccount(account);
+        List<Account> accounts = customer.getAccounts();
+        assertEquals(1, accounts.size());
+        assertEquals("acc1", accounts.get(0).getAccountId());
     }
 }
