7d84209a9459694375a386327e9baaae0ac0f4b0
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-12-02 21:15:29 +0100

    RED: Add failing test for transfer method in Bank class

--- app/src/test/java/bankapp/BankTest.java
+++ app/src/test/java/bankapp/BankTest.java
@@ -1,14 +1,39 @@
 package bankapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class BankTest {
+    private Bank bank;
+    private Customer customer;
+    private Account account1;
+    private Account account2;
+
+    @Before
+    public void setUp() {
+        bank = new Bank();
+        customer = new Customer("cust1");
+        account1 = new Account("acc1");
+        account2 = new Account("acc2");
+        customer.addAccount(account1);
+        customer.addAccount(account2);
+        bank.addCustomer(customer);
+    }
+
     @Test
     public void testAddCustomer() {
-        Bank bank = new Bank();
-        Customer customer = new Customer("cust1");
-        bank.addCustomer(customer);
-        assertEquals(customer, bank.getCustomer("cust1"));
+        Customer newCustomer = new Customer("cust2");
+        bank.addCustomer(newCustomer);
+        assertEquals(newCustomer, bank.getCustomer("cust2"));
+    }
+
+    @Test
+    public void testTransfer() {
+        account1.deposit(100.0);
+        bank.transfer("acc1", "acc2", 50.0);
+        assertEquals(50.0, account1.getBalance(), 0.01);
+        assertEquals(50.0, account2.getBalance(), 0.01);
     }
 }
