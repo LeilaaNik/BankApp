3bbeb95d8f90144815bd3c4cd2b06fa58d089894
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-12-04 21:31:24 +0100

    REFACTOR: Consolidate tests into a single method in TransactionTest class

--- app/src/test/java/bankapp/TransactionTest.java
+++ app/src/test/java/bankapp/TransactionTest.java
@@ -5,26 +5,11 @@ import static org.junit.Assert.*;
 
 public class TransactionTest {
     @Test
-    public void testTransactionId() {
-        Transaction transaction = new Transaction("txn1", null, null, 0.0);
-        assertEquals("txn1", transaction.getTransactionId());
-    }
-
-    @Test
-    public void testFromAccountId() {
-        Transaction transaction = new Transaction("txn1", "acc1", null, 0.0);
-        assertEquals("acc1", transaction.getFromAccountId());
-    }
-
-    @Test
-    public void testToAccountId() {
-        Transaction transaction = new Transaction("txn1", "acc1", "acc2", 0.0);
-        assertEquals("acc2", transaction.getToAccountId());
-    }
-
-    @Test
-    public void testAmount() {
+    public void testTransactionCreation() {
         Transaction transaction = new Transaction("txn1", "acc1", "acc2", 100.0);
+        assertEquals("txn1", transaction.getTransactionId());
+        assertEquals("acc1", transaction.getFromAccountId());
+        assertEquals("acc2", transaction.getToAccountId());
         assertEquals(100.0, transaction.getAmount(), 0.01);
     }
 }
