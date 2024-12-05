9f542c004468621ea279a497ebedea9a7cdbbee4
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-12-05 23:01:27 +0100

    RED: Add failing test for addCustomer method in App class

---
+++ app/src/test/java/bankapp/AppTest.java
@@ -0,0 +1,32 @@
+package bankapp;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class AppTest {
+    private Bank bank;
+    private TransactionService transactionService;
+    private App app;
+
+    @Before
+    public void setUp() {
+        bank = mock(Bank.class);
+        transactionService = mock(TransactionService.class);
+        app = new App(bank, transactionService);
+    }
+
+    @Test
+    public void testAddCustomer() {
+        String input = "cust1\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Scanner scanner = new Scanner(System.in);
+        app.addCustomer(scanner);
+        verify(bank).addCustomer(any(Customer.class));
+    }
+}
